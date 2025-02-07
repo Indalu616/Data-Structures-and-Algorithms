864076ceddf88bd190bf8bc0ab7c881863b76144
Indalu Kelbesa <156249045+Indalu616@users.noreply.github.com>
2025-02-07 23:13:06 +0400

    Created 'Missing Number' by A2SV Hub

---
+++ 07-Feb-2025/Missing Number 250725.py
@@ -0,0 +1,15 @@
+# Problem: Missing Number - https://leetcode.com/problems/missing-number/description/
+
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        n=len(nums)
+
+        for i in range(n+1):
+            if i not in nums:
+                return i
+        
