520cb05b702759468dbfcf6d4aa681670c257d1e
Indalu Kelbesa <156249045+Indalu616@users.noreply.github.com>
2025-02-09 11:48:10 +0400

    Created 'Same Differences' by A2SV Hub

---
+++ 09-Feb-2025/Same Differences 251870.py
@@ -0,0 +1,16 @@
+# Problem: Same Differences - https://codeforces.com/problemset/problem/1520/D
+
+t=int(input())
+for i in range(t):
+    n=int(input())
+    nums=list(map(int,input().split()))
+    freq={}
+    count=0
+    for j in range(n):
+        key=nums[j]-j
+        if key in freq:
+            freq[key]+=1
+            count+=freq[key]
+        else:
+            freq[key]=0
+    print(count)
