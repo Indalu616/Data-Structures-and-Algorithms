4b2c7daa1e146d4bb6c82c505f8d27f6355923f8
Indalu Kelbesa <156249045+Indalu616@users.noreply.github.com>
2025-02-10 22:08:34 +0400

    Created 'C - Sura loves coding' by A2SV Hub

---
+++ A2SV Remote Education Contest #2 10-Feb-2025/C - Sura loves coding 254243.py
@@ -0,0 +1,22 @@
+# Problem: C - Sura loves coding - https://codeforces.com/gym/586622/problem/C
+
+n=int(input())
+
+encoded=input()
+
+decoded=""
+
+if n%2:
+    for i in range(n):
+        if i%2:
+            decoded=encoded[i]+decoded
+        else:
+            decoded+=encoded[i]
+else:
+    for i in range(n):
+        if i%2:
+            decoded+=encoded[i]
+        else:
+            decoded=encoded[i]+decoded
+            
+print(decoded)
