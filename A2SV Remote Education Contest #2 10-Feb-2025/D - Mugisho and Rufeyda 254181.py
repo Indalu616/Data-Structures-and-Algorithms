d188e86752e05119b940579b14b1ec71bad21983
Indalu Kelbesa <156249045+Indalu616@users.noreply.github.com>
2025-02-10 21:32:42 +0400

    Created 'D - Mugisho and Rufeyda' by A2SV Hub

---
+++ A2SV Remote Education Contest #2 10-Feb-2025/D - Mugisho and Rufeyda 254181.py
@@ -0,0 +1,20 @@
+# Problem: D - Mugisho and Rufeyda - https://codeforces.com/gym/586622/problem/D
+
+nums=list(map(int,input().split()))
+
+n=nums[0]
+t=nums[1]
+
+lower=10**(n-1)
+upper=10**n
+is_found=False
+
+while lower < upper:
+    if lower % t==0 and lower >0:
+        print(lower)
+        is_found=True
+        break
+    lower+=1
+
+if not is_found:
+    print(-1)
