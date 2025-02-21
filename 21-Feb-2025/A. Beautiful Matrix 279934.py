a1dc3322c45a606c9c69575dc458deeb2ed62c98
Indalu Kelbesa <156249045+Indalu616@users.noreply.github.com>
2025-02-22 00:15:22 +0400

    Created 'A. Beautiful Matrix' by A2SV Hub

---
+++ 21-Feb-2025/A. Beautiful Matrix 279934.py
@@ -0,0 +1,14 @@
+# Problem: A. Beautiful Matrix - https://codeforces.com/problemset/problem/263/A
+
+for i in range(5):
+    
+    nums=list(map(int,input().split()))
+    is_found=False
+    
+    for j in range(len(nums)):
+        if nums[j]==1:
+            print(abs(i-2)+abs(j-2))
+            is_found=True
+            break
+    if is_found:
+        break
