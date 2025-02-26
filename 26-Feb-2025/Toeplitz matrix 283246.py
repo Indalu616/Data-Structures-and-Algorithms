621f74b347fcba70bd83e58721510f010aec0dd9
Indalu Kelbesa <156249045+Indalu616@users.noreply.github.com>
2025-02-26 15:15:14 +0400

    Created 'Toeplitz matrix' by A2SV Hub

---
+++ 26-Feb-2025/Toeplitz matrix 283246.py
@@ -0,0 +1,23 @@
+# Problem: Toeplitz matrix - https://leetcode.com/problems/toeplitz-matrix/description/
+
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+
+        
+
+        is_toe=True
+    
+
+        for i in range(1,len(matrix)):
+            for j in range(1,len(matrix[0])):
+
+                if matrix[i-1][j-1]!=matrix[i][j]:
+                    is_toe=False
+        
+
+        return is_toe
+
+
+
+
+        
