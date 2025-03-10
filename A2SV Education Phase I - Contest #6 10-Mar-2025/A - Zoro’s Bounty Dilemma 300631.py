c7fd292bb191ab2c42cd565461f91fa58a8ad160
Indalu Kelbesa <156249045+Indalu616@users.noreply.github.com>
2025-03-10 21:46:42 +0400

    Created 'A - Zoro’s Bounty Dilemma' by A2SV Hub

---
+++ A2SV Education Phase I - Contest #6 10-Mar-2025/A - Zoro’s Bounty Dilemma 300631.py
@@ -0,0 +1,25 @@
+# Problem: A - Zoro’s Bounty Dilemma - https://codeforces.com/gym/594356/problem/A
+
+def determine_bounty_relationship(test_cases):
+    results = []
+    for s in test_cases:
+        has_less = '<' in s
+        has_greater = '>' in s
+        
+        if has_less and has_greater:
+            results.append('?')
+        elif has_less:
+            results.append('<')
+        elif has_greater:
+            results.append('>')
+        else:
+            results.append('=')
+    
+    return results
+
+# Read input
+t = int(input())
+test_cases = [input().strip() for _ in range(t)]
+
+for result in determine_bounty_relationship(test_cases):
+    print(result)
