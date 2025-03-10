75e636b9b6b7ea39dabd384c573d081c11117471
Indalu Kelbesa <156249045+Indalu616@users.noreply.github.com>
2025-03-10 19:44:36 +0400

    Created 'B - Thousand Sunny's Network Setup' by A2SV Hub

---
+++ A2SV Education Phase I - Contest #6 10-Mar-2025/B - Thousand Sunny's Network Setup 300457.py
@@ -0,0 +1,12 @@
+# Problem: B - Thousand Sunny's Network Setup - https://codeforces.com/gym/594356/problem/B
+
+def max_internet_speed(n, k, speeds):
+    speeds.sort(reverse=True) 
+    return speeds[k-1] 
+
+
+n, k = map(int, input().split())
+speeds = list(map(int, input().split()))
+
+
+print(max_internet_speed(n, k, speeds))
