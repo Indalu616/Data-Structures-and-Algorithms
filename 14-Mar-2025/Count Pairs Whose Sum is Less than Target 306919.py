75e413be6f695590a39e210c074049a2536fed5b
Indalu Kelbesa <156249045+Indalu616@users.noreply.github.com>
2025-03-14 19:59:14 +0400

    Created 'Count Pairs Whose Sum is Less than Target' by A2SV Hub

---
+++ 14-Mar-2025/Count Pairs Whose Sum is Less than Target 306919.py
@@ -0,0 +1,20 @@
+# Problem: Count Pairs Whose Sum is Less than Target - https://leetcode.com/problems/count-pairs-whose-sum-is-less-than-target/
+
+class Solution(object):
+    def countPairs(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+
+        count=0
+
+        for i in range(len(nums)-1):
+
+            for j in range(i+1,len(nums)):
+                 if nums[i]+nums[j] < target:
+                    count+=1
+        return count
+
+        
