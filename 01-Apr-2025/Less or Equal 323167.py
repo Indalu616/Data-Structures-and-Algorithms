920005e8273bd0dd97f8a414d14294db6aad5881
Indalu Kelbesa <156249045+Indalu616@users.noreply.github.com>
2025-04-01 16:27:44 +0400

    Created 'Less or Equal' by A2SV Hub

---
+++ 01-Apr-2025/Less or Equal 323167.py
@@ -0,0 +1,25 @@
+# Problem: Less or Equal - https://codeforces.com/problemset/problem/977/C
+
+
+
+n,k=map(int,input().split())
+
+arr=list(map(int,input().split()))
+
+arr.sort()
+
+
+if k == 0:
+    print(1 if arr[0] > 1 else -1 ) 
+    
+elif k < n and arr[k - 1] == arr[k]:  
+    print(-1) 
+else:
+    print(arr[k - 1]) 
+
+
+
+
+    
+        
+        
