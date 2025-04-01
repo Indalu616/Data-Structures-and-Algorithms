110e135c752584c5b39e840505be6d5c3fa5d7e7
Indalu Kelbesa <156249045+Indalu616@users.noreply.github.com>
2025-04-01 15:48:58 +0400

    Created 'Books' by A2SV Hub

---
+++ 01-Apr-2025/Books 323148.py
@@ -0,0 +1,27 @@
+# Problem: Books - https://codeforces.com/contest/279/problem/B
+
+
+
+n,t=map(int,input().split())
+
+a=list(map(int,input().split()))
+
+
+current_sum=0
+max_count=0
+
+left=0
+
+for right in range(n):
+    
+    current_sum+=a[right]
+    
+    while current_sum > t:
+        current_sum-=a[left]
+        left+=1
+    
+    max_count=max(max_count,right-left+1)
+print(max_count)
+        
+        
+        
