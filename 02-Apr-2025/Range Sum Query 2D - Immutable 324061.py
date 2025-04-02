ec4a8ab18c5cdcd929a7a7e3b999609d6b6f33fe
Indalu Kelbesa <156249045+Indalu616@users.noreply.github.com>
2025-04-02 19:38:16 +0400

    Created 'Range Sum Query 2D - Immutable' by A2SV Hub

---
+++ 02-Apr-2025/Range Sum Query 2D - Immutable 324061.py
@@ -0,0 +1,46 @@
+# Problem: Range Sum Query 2D - Immutable - https://leetcode.com/problems/range-sum-query-2d-immutable/
+
+class NumMatrix(object):
+
+    def __init__(self, matrix):
+        """
+        :type matrix: List[List[int]]
+
+        """
+        if not matrix or not matrix[0]:
+            return 
+        
+        m, n = len(matrix), len(matrix[0])
+        self.prefixSum = [[0] * (n + 1) for _ in range(m + 1)]
+        
+        # Compute prefix sum
+        for i in range(m):
+            for j in range(n):
+                self.prefixSum[i+1][j+1] = (
+                    matrix[i][j] 
+                    + self.prefixSum[i][j+1] 
+                    + self.prefixSum[i+1][j] 
+                    - self.prefixSum[i][j]
+                )
+
+        
+
+    def sumRegion(self, row1, col1, row2, col2):
+        """
+        :type row1: int
+        :type col1: int
+        :type row2: int
+        :type col2: int
+        :rtype: int
+        """
+        return (
+            self.prefixSum[row2+1][col2+1] 
+            - self.prefixSum[row1][col2+1] 
+            - self.prefixSum[row2+1][col1] 
+            + self.prefixSum[row1][col1]
+        )
+        
+
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)
