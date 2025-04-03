a8cd3f7feb9cc3a54ac4612cac8110bc36f393db
Indalu Kelbesa <156249045+Indalu616@users.noreply.github.com>
2025-04-03 22:31:04 +0400

    Created 'Sereja and Dima' by A2SV Hub

---
+++ 03-Apr-2025/Sereja and Dima 325390.py
@@ -0,0 +1,37 @@
+# Problem: Sereja and Dima - https://codeforces.com/problemset/problem/381/A
+
+
+n=int(input())
+
+nums=list(map(int,input().split()))
+
+left=0
+
+right=len(nums)-1
+
+sereja=0
+dima=0
+turn=1
+
+while left <= right:
+    
+    if turn %2:
+        if nums[left] >=nums[right]:
+            dima+=nums[left]
+            left+=1
+            turn+=1
+        else:
+            dima+=nums[right]
+            right-=1
+            turn+=1
+    else:
+        if nums[left] >=nums[right]:
+            sereja+=nums[left]
+            left+=1
+            turn+=1
+        else:
+            sereja+=nums[right]
+            right-=1
+            turn+=1
+print(dima, sereja)
+        
