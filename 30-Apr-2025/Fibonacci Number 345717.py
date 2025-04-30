877e938c74bd7a0e0904e4ddf4a943955fa17b4a
Indalu Kelbesa <156249045+Indalu616@users.noreply.github.com>
2025-04-30 19:59:47 +0400

    Created 'Fibonacci Number' by A2SV Hub

---
+++ 30-Apr-2025/Fibonacci Number 345717.py
@@ -0,0 +1,17 @@
+# Problem: Fibonacci Number - https://leetcode.com/problems/fibonacci-number/
+
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+       
+
+        if n==1:
+            return 1
+        elif n==0:
+            return 0
+        else:
+            return self.fib(n-1)+self.fib(n-2)
+        
